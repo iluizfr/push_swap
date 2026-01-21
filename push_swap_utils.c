7e413ca50520ec5c9064484c0066f7d9c650f02f
Luis Felipe De Freitas <lde-frei@c1r5s12.42lisboa.com>
2026-01-21 17:44:14 +0000

    ..

--- push_swap_utils.c
+++ push_swap_utils.c
@@ -6,8 +6,32 @@
 /*   By: lde-frei <lde-frei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/07 16:32:52 by lde-frei          #+#    #+#             */
-/*   Updated: 2026/01/20 12:01:57 by lde-frei         ###   ########.fr       */
+/*   Updated: 2026/01/21 17:38:56 by lde-frei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+
+int		is_sorted(t_list *stack)
+{
+	t_list *tmp;
+
+	tmp = stack;
+	while (tmp)
+	{
+		
+	}
+}
+
+void	sort_stack(t_list **stack_a, t_list **stack_b)
+{
+	int	last;
+	int	bigger;
+
+	
+	while (!is_sorted(*stack_a))
+	{
+		
+	}
+}
